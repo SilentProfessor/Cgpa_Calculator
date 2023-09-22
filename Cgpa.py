6a35ecbfaeee6b17c13416fbb28699cc245e1e5d
SilentProfessor <142599405+SilentProfessor@users.noreply.github.com>
2023-09-22 17:43:15 +0530

    Cgpa.py
    Communicative Grade point script

---
+++ Cgpa.py
@@ -0,0 +1,63 @@
+print("                                                                  ⏳️    Rr Satyamev Jayate   ⏳            ")
+print("                                                             ✡             Silent Professor             ✡️ ")
+print()
+
+
+
+A= int(input("Enter Grade point sub A :  "))
+B=int(input("Enter Grade point sub B  :  "))
+C=int(input("Enter Grade point sub C  :  "))
+D=int(input("Enter Grade point sub D  :  "))
+E=int(input("Enter Grade point sub E :  "))
+F=int(input("Enter Grade point sub F :  "))
+G=int(input("Enter Grade point sub G  :  "))
+I=int(input("Enter Grade point sub H :  "))
+J=int(input("Enter Grade point sub I :  "))
+K=int(input("Enter Grade point sub J  :  "))
+L=int(input("Enter Grade point sub K  :  "))
+M=int(input("Enter Grade point sub L  :  "))
+
+
+print()
+
+a=float(input("Enter Credit Point sub A  :  "))
+b=float(input("Enter Credit Point sub B  :  "))
+c=float(input("Enter Credit Point sub C  :  "))
+d=float(input("Enter Credit Point sub D  :  "))
+e=float(input("Enter Credit Point sub E  :  "))
+f=float(input("Enter Credit Point sub F :  "))
+g=float(input("Enter Credit Point sub G  :  "))
+i=float(input("Enter Credit Point sub H  :  "))
+j=float(input("Enter Credit Point sub I  :  "))
+k=float(input("Enter Credit Point sub J  :  "))
+l=float(input("Enter Credit Point sub K  :  "))
+m=float(input("Enter Credit Point sub L  :  "))
+
+print()
+
+TotalGpa =((A*a)+(B*b)+(C*c)+(D*d)+(E*e)+(F* f)+(G*g)+I*i+J*j+K*k+L*l+M*m)
+TotalCpa =(a+b+c+d+e+f+g+i+j+k+l+m)
+CGPA=(TotalGpa/TotalCpa)
+v=str(input(" Name  :  "))
+R=input(" Registration :  ")
+z=input(" Current Semester No : ")
+print()
+print("Current Semester GPA  :  ",CGPA)
+
+print()
+p=CGPA*9.5
+print("Current Semester Percentage  : ",p)
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+print(input("                                                                 $  Craeted By  Er. RAHUL KUMAR JHA   $  "))
+
+
